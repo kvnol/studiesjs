61bca27a41f0f3950352c404165b3dd540dd8e3a
Kevin Oliveira <me@kevinoliveira.com.br>
2018-03-13 21:59:31 -0300

    Study event 'onhashchange' complete

---
+++ events/onhashchange/main.js
@@ -0,0 +1,14 @@
+((win, doc) => {
+  'use strict';
+
+  var item = doc.querySelectorAll('.item');
+  console.log(item);
+
+  win.addEventListener('hashchange', () => {
+    item.forEach((item) => {
+      if (location.hash.endsWith(item.id))
+        return item.style.background = item.id;
+      return item.style.background = '#e5e5e5';
+    });
+  }, false);
+})(window, document);
